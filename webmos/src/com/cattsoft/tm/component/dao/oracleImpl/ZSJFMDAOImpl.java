7735e12678058a3530d5a7122724862a2ddf9ded
xieyunchaobest@gmail.com <xieyunchaobest@gmail.com>
2015-02-01 21:26:57 +0800

    edit config

--- webmos/src/com/cattsoft/tm/component/dao/oracleImpl/ZSJFMDAOImpl.java
+++ webmos/src/com/cattsoft/tm/component/dao/oracleImpl/ZSJFMDAOImpl.java
@@ -2696,29 +2696,14 @@ public class ZSJFMDAOImpl implements IZSJFMDAO {
 			
 			String khq=(String)m.get("khq");
 			String hylx=(String)m.get("hylx");
-			if("全部".equals(khq) && "全部".equals(hylx) ) {
-				sql.append(" and KHQ=:khq and HYLX=:hylx");
-				sql.setString("khq", (String)m.get("khq"));
-				sql.setString("hylx", (String)m.get("hylx"));
-			}else {
-				if("全部".equals(khq)) khq="";
-				if("全部".equals(hylx))hylx="";
-			}
-
-			if (StringUtil.isBlank(khq)
-					 ) {
-			} else {
-				sql.append(" and KHQ=:khq");
-				sql.setString("khq", khq);
-			}
+			if(StringUtil.isBlank(khq)) khq="全部";
+			if(StringUtil.isBlank(hylx)) hylx="全部";
+			 
+			sql.append(" and KHQ=:khq");
+			sql.setString("khq", khq);
 			
-			if (StringUtil.isBlank(hylx)
-					) {
-			} else {
-				sql.append(" and HYLX=:hylx");
-				sql.setString("hylx", (String)m.get("hylx"));
-			}
-			
+			sql.append(" and HYLX=:hylx");
+			sql.setString("hylx", hylx);
 
 			conn = ConnectionFactory.getConnection();
 			ps = conn.prepareStatement(sql.getSql());
